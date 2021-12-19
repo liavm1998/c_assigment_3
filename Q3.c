befab5f083944de64e8f77b6e8c7796be01d2a20
liavm1998 <liavmoshe12345@gmail.com>
2021-12-19 16:09:59 +0200

    Update Q3.c

--- Q3.c
+++ Q3.c
@@ -6,53 +6,69 @@
 #define TXT 1024
 #define WORD 30
 
-
-char* Q3(char* w,char* txt)
+char *Q3(char *w, char *txt)
 {
     ////////ans dec
-    char *ans= malloc(sizeof(char) * 780);
+    char *ans = malloc(sizeof(char) * 780);
     memset(ans, 0, strlen(ans));
-    
+
     /////////pointers declaration
-    int s_pointer=0; 
+    int s_pointer = 0;
     ///////////checker dec
-    char*copy=malloc(sizeof(char) * strlen(w));
-    strcpy(copy,w);
+    char *copy = malloc(sizeof(char) * strlen(w));
+    strcpy(copy, w);
     /////////running thru the txt
-    int stop=strlen(txt);
-    for (int i = 0;i<stop;i++) 
-    {    
-        char cur=txt[i];
-        if(cur==' ')
+    int stop = strlen(txt);
+    for (int i = 0; i < stop; i++)
+    {
+        char cur = txt[i];
+        if (cur == ' ')
         {
             continue;
         }
+        if (cur == 'b')
+        {
+            cur = 'b';
+        }
         //////////char is currect count it
-        if(strchr(copy,cur)!=NULL)
+        if (strchr(copy, cur) != NULL)
         {
             ///////delete cur char from copy
-            deletechar(copy,cur);
+            deletechar(copy, cur);
         }
         else
         {
-            s_pointer=i;
-            strcpy(copy,w);
-        }
-        if ((copy != NULL) && (copy[0] == '\0'))//////////we fount word
-        {
-            char* concat= malloc(sizeof(char) * 780);
-            strncpy(concat,txt+s_pointer+1,i-s_pointer);
-            cur=concat[0];
-            while(cur==' ')
+            if (strchr(w, cur) != NULL)
             {
-                deletechar(concat,cur);
-                cur=concat[0];
+                s_pointer++;
+                ///////delete cur char from copy
+                i=s_pointer;
+                //deletechar(copy, cur);
             }
-            strcat(ans,concat);
-            strcat(ans,"~");
-            s_pointer=i;
+            else
+            {
+                s_pointer = i;
+            }
+            
+            strcpy(copy, w);
+            
+        }
+        if ((copy != NULL) && (copy[0] == '\0')) //////////we fount word
+        {
+            char *concat = malloc(sizeof(char) * 780);
+            strncpy(concat, txt + s_pointer + 1, i - s_pointer);
+            cur = concat[0];
+            while (cur == ' ')
+            {
+                deletechar(concat, cur);
+                cur = concat[0];
+            }
+            strcat(ans, concat);
+            strcat(ans, "~");
+            s_pointer = i;
+            strcpy(copy, w);
         }
     }
     ans[strlen(ans) - 1] = '\0';
-    return ans;   
+    return ans;
 }
